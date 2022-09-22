6f4e10f8e4b5dd3afa1809d06677ffacf29ab5c5
gabriel huene <gabriellaurence05@gmail.com>
2022-09-22 11:56:33 -0300

    adiciona a solução do exercício nomeninja

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+Console . Write( " texto a ser traduzido : " );
+Console . WriteLine (
+Console . ReadLine ()    
+
+.Replace ( "a" , "KA" )
+.Replace ( "b" , "ZU" )
+.Replace ( "c" , "MI" )
+.Replace ( "d" , "TE" )
+.Replace ( "e" , "KU" )
+.Replace ( "f" , "LU" )
+.Replace ( "g" , "JI" )
+.Replace ( "h" , "RI" )
+.Replace ( "i" , "KI" )
+.Replace ( "j" , "ZUS" )
+.Replace ( "k" , "ME" )
+.Replace ( "l" , "TA" )
+.Replace ( "m" , "RIN" )
+.Replace ( "n" , "PARA" )
+.Replace ( "o" , "MO" )
+.Replace ( "p" , "NÃO" )
+.Replace ( "q" , "KE")
+.Replace ( "r" , "SHI" )
+.Replace ( "s" , "ARI" )
+.Replace ( "t" , "CHI" )
+.Replace ( "u" , "DO" )
+.Replace ( "v" , "RV" )
+.Replace ( "w" , "MEI" )
+.Replace ( "x" , "NA" )
+.Replace ( "y" , "FV" )
+.Replace ( "z" , "ZI" )
+);
